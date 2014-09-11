74e6c5892c26479be9cbbe011a04ba133078fa32
Ibara Mayaka <juliodion_12345@hotmail.com>
2014-09-10 21:55:17 -0600

    Eliminados mas archivos autogenerados en CordovaLib

--- DuckRaid/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/mayaka/projects/Fungae/DuckRaid/DuckRaid/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/mayaka/projects/Fungae/DuckRaid/DuckRaid/platforms/android/CordovaLib/AndroidManifest.xml \
--- DuckRaid/platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/mayaka/projects/Fungae/DuckRaid/DuckRaid/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
